3d324541c63e027d9815191a05e4f90f8623653e
Evan_air <youngp513@163.com>
2021-03-28 14:37:47 +0800

    update build script for mac

--- build.sh
+++ build.sh
@@ -1,19 +1,2 @@
 #/bin/bash
-set -e
-OUTPUT="output"
-if [ ! -e output ]; then
-  mkdir $OUTPUT
-fi
-
-UNAME=$(uname -s)
-if [ $UNAME == "Linux" ] || [ $UNAME == "Darwin" ]; then
-  LATEX="xelatex"
-else
-  LATEX="pdflatex"
-fi
-echo "Building with "$LATEX" on "$UNAME
-"$LATEX" -output-directory "$OUTPUT" shu-thesis.tex
-bibtex shu-thesis.aux
-"$LATEX" -output-directory "$OUTPUT" shu-thesis.tex
-"$LATEX" -output-directory "$OUTPUT" shu-thesis.tex
-
+latexmk -xelatex -output-directory="./out" shu-thesis.tex 
